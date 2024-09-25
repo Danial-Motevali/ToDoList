8b617c05d2da500530fd35017ec9230555a64b57
Danial-Motevali <danial.motevali.82@gmail.com>
2024-09-25 14:18:08 +0330

    add the setting section in application and also IdentitySetting entity

---
+++ ToDoList.Application/Settings/IdentitySetting.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoList.Application.Settings
+{
+    public class IdentitySetting
+    {
+        public string SigningKey { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Subject { get; set; }
+    }
+}
