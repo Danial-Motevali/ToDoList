9a5bf446517eab11c3af313d137be950c181e68c
Danial-Motevali <danial.motevali.82@gmail.com>
2024-09-25 14:34:55 +0330

    start to work in TokenUtility

---
+++ ToDoList.Identity/Utilities/TokenUtilty.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using ToDiList.Domain.Model.Security;
+
+namespace ToDoList.Identity.Utilities
+{
+    public class TokenUtilty
+    {
+        public string CreateToken(/*ApplicationUserDto user*/)
+        {
+            //List<Claim> claims = new()
+            //{
+            // new Claim(ClaimTypes.Name, user.UserName),
+            // new Claim(ClaimTypes.Role, "Admin")
+            //};
+
+            //var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSetting:Token").Value!));
+
+            //var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            //var token = new JwtSecurityToken(
+            //    _tokenSetting.Issuer,
+            //    _tokenSetting.Audience,
+            //    claims: claims,
+            //    expires: DateTime.Now.AddDays(1),
+            //    signingCredentials: cred
+            // );
+
+            //var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            //return jwt;
+
+            return "";
+        }
+    }
+}
