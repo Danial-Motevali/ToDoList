3eed4639b5fbbccc113818668bdde4b050ee888c
Danial Motevali <danial.motevali.82@gmail.com>
2024-09-27 21:05:25 +0330

    create a base controller

---
+++ ToDoList.Server/Controllers/Base/BaseController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query;
+using ToDiList.Domain.ResultModel;
+
+namespace ToDoList.EndPoint.Controllers.Base
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+
+        protected ActionResult ResultHandler<T>(Result<T> result)
+        {
+            //How Fail Should be
+            if (result.IsSucces == false && !String.IsNullOrEmpty(result.ErrorMessage))
+                return Ok(result);
+
+            //How Success should be
+            if(result.IsSucces && result.Data != null)
+                return Ok(result);
+
+            return BadRequest();
+        }
+    }
+}
