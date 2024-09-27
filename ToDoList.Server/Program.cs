9ba8cbad509ba1525eabd7bcb409c647455883ba
Danial Motevali <danial.motevali.82@gmail.com>
2024-09-27 21:05:03 +0330

    .

--- ToDoList.Server/Program.cs
+++ ToDoList.Server/Program.cs
@@ -20,6 +20,7 @@ builder.Services.AddPersistenseServices(builder.Configuration);
 
 #endregion
 
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
