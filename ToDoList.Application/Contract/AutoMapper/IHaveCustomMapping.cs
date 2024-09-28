205e2331cfa7f342b4097adee4657bac21c5436d
Danial Motevali <danial.motevali.82@gmail.com>
2024-09-28 23:43:54 +0330

    Create a cantract for mapper profile

---
+++ ToDoList.Application/Contract/AutoMapper/IHaveCustomMapping.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace ToDoList.Application.Contract.AutoMapper
+{
+    public interface IHaveCustomMapping
+    {
+        void CreateCustomMapping(Profile profile);
+    }
+}
