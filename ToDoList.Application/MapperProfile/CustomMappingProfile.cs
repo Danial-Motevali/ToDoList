dbaa7b0fb8d09f18afb78c05c87b4be61679bb01
Danial Motevali <danial.motevali.82@gmail.com>
2024-09-28 23:47:41 +0330

    find all the IHaveCustomMapping and map them

---
+++ ToDoList.Application/MapperProfile/CustomMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ToDoList.Application.Contract.AutoMapper;
+
+namespace ToDoList.Application.MapperProfile
+{
+    public class CustomMappingProfile : Profile
+    {
+        public CustomMappingProfile(IEnumerable<IHaveCustomMapping> mappings)
+        {
+            foreach (var mapping in mappings)
+            {
+                mapping.CreateCustomMapping(this);
+            }
+        }
+    }
+}
