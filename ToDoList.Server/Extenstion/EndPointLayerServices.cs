d5c0a5743fc3bc04f95c30455316c1eb2f3ff8b9
Danial Motevali <danial.motevali.82@gmail.com>
2024-09-28 23:52:54 +0330

    add services in Extention method

--- ToDoList.Server/Extenstion/EndPointLayerServices.cs
+++ ToDoList.Server/Extenstion/EndPointLayerServices.cs
@@ -1,9 +1,37 @@
-﻿using System.Reflection;
+﻿using AutoMapper;
+using System.Reflection;
+using ToDoList.Application.Contract.AutoMapper;
+using ToDoList.Application.MapperProfile;
 
 namespace ToDoList.EndPoint.Extenstion
 {
     public static class EndPointLayerServices
     {
-        
+        public static void AddEndPointServices(this IServiceCollection services, params Assembly[] assemblies)
+        {
+            services.AddAutoMapper(config =>
+            {
+                config.AddCustomMappingProfile();
+            }, assemblies);
+        }
+
+
+        public static void AddCustomMappingProfile(this IMapperConfigurationExpression config)
+        {
+            config.AddCustomMappingProfile(AppDomain.CurrentDomain.GetAssemblies().Where(c => c.FullName.StartsWith("ToDoList.Application")).ToArray());
+        }
+
+        public static void AddCustomMappingProfile(this IMapperConfigurationExpression config, params Assembly[] assemblies)
+        {
+            var allTypes = assemblies.SelectMany(a => a.ExportedTypes);
+
+            var list = allTypes.Where(type => type.IsClass && !type.IsAbstract &&
+                type.GetInterfaces().Contains(typeof(IHaveCustomMapping)))
+                .Select(type => (IHaveCustomMapping)Activator.CreateInstance(type));
+
+            var profile = new CustomMappingProfile(list);
+
+            config.AddProfile(profile);
+        }
     }
 }
