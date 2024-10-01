7c99a425fff6982aebba89cdcbd9e00a94531040
Danial Motevali <danial.motevali.82@gmail.com>
2024-10-01 21:45:55 +0330

    add usable message for appliction

---
+++ ToDiList.Domain/ErrorMessage/SecurityErrorMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDiList.Domain.ErrorMessage
+{
+    public static class SecurityErrorMessage
+    {
+        public static string CantRegisterTheUser = "there is some thing wring with with services";
+
+        public static string NoUniquieValue => "There is some one with this Enfo plese sign-in" ;
+    }
+}
