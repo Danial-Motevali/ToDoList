1c1c011b51469ceb35e6cf1fd356a521fa679156
Danial-Motevali <danial.motevali.82@gmail.com>
2024-10-01 17:02:13 +0330

    start to create registerCommand

--- ToDoList.Application/Feature/Handler/SecurityHandler/RegisterCommandHandler.cs
+++ ToDoList.Application/Feature/Handler/SecurityHandler/RegisterCommandHandler.cs
@@ -1,13 +1,33 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
+using ToDiList.Domain.Model.Security;
+using ToDiList.Domain.Repository;
 using ToDiList.Domain.ResultModel;
+using ToDoList.Application.DTO.ModelDto;
 using ToDoList.Application.Feature.Command.SecurityFeatrue;
 
 namespace ToDoList.Application.Feature.Handler.SecurityHandler
 {
     public class RegisterCommandHandler : IRequestHandler<RegisterCommand, Result<bool>>
     {
+        private readonly IRepository<ApplicationUser> _userRepo;
+        private readonly IMapper _mapper;
+        public RegisterCommandHandler
+            (
+                IRepository<ApplicationUser> userRepo,
+                IMapper mapper
+            )
+        {
+            _userRepo = userRepo;
+            _mapper = mapper;
+        }
+
         public Task<Result<bool>> Handle(RegisterCommand request, CancellationToken cancellationToken)
         {
+            var dto = new ApplicationUserDto();
+
+            ApplicationUser newUser = _mapper.Map<ApplicationUser>(dto);
+
             throw new NotImplementedException();
         }
     }
