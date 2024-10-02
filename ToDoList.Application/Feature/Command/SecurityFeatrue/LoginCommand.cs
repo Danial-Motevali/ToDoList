ec46089b013f8acc9a6e6b855d1868adc57e2b99
Danial-Motevali <danial.motevali.82@gmail.com>
2024-10-02 10:04:03 +0330

    create a CQRS architecture for login end point

---
+++ ToDoList.Application/Feature/Command/SecurityFeatrue/LoginCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using ToDiList.Domain.ResultModel;
+using ToDoList.Application.DTO.Base_Dto;
+using ToDoList.Application.DTO.RequestDto;
+
+namespace ToDoList.Application.Feature.Command.SecurityFeatrue
+{
+    public class LoginCommand : BaseDto<LoginRequest, LoginCommand>, IRequest<Result<string>>
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ToDoList.Application/Feature/Handler/SecurityHandler/LoginCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using ToDiList.Domain.ResultModel;
+using ToDoList.Application.Feature.Command.SecurityFeatrue;
+
+namespace ToDoList.Application.Feature.Handler.SecurityHandler
+{
+    public class LoginCommandHandler : IRequestHandler<LoginCommand, Result<string>>
+    {
+        public Task<Result<string>> Handle(LoginCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
