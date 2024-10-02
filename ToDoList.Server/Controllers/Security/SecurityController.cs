ad9cfd571cd760407a5793cd0bdee1e359562795
Danial-Motevali <danial.motevali.82@gmail.com>
2024-10-02 10:04:33 +0330

    create a end point and call the command

--- ToDoList.Server/Controllers/Security/SecurityController.cs
+++ ToDoList.Server/Controllers/Security/SecurityController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ToDoList.Application.DTO.EndPointsDto;
+using ToDoList.Application.DTO.RequestDto;
 using ToDoList.Application.Feature.Command.SecurityFeatrue;
 using ToDoList.EndPoint.Controllers.Base;
 
@@ -17,6 +18,19 @@ namespace ToDoList.EndPoint.Controllers.Security
                     await Mediator.Send
                     (
                         _mapper.Map<RegisterCommand>(input)
+                    )
+                );
+        }
+
+        [HttpPost]
+        [Route("sign-in")]
+        public async Task<IActionResult> Login(LoginRequest input)
+        {
+            return ResultHandler
+                (
+                    await Mediator.Send
+                    (
+                        _mapper.Map<LoginCommand>(input)
                     )
                 );
         }
