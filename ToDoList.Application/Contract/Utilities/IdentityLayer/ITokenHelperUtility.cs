f42fbed09df34473081725d1053a78dfd103716e
Danial-Motevali <danial.motevali.82@gmail.com>
2024-10-02 10:36:32 +0330

    create s funcinality for create a token

---
+++ ToDoList.Application/Contract/Utilities/IdentityLayer/ITokenHelperUtility.cs
@@ -0,0 +1,9 @@
+﻿using ToDiList.Domain.Model.Security;
+
+namespace ToDoList.Application.Contract.Utilities.IdentityLayer
+{
+    public interface ITokenHelperUtility
+    {
+        public string CreateToken(ApplicationUser user);
+    }
+}
--- ToDoList.Identity/Utilities/TokenHelperUtilty.cs
+++ ToDoList.Identity/Utilities/TokenHelperUtilty.cs
@@ -1,37 +1,45 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using ToDiList.Domain.Model.Security;
+using ToDoList.Application.Contract.Utilities.IdentityLayer;
+using ToDoList.Application.Settings;
 
 namespace ToDoList.Identity.Utilities
 {
-    public class TokenHelperUtilty
+    public class TokenHelperUtilty : ITokenHelperUtility
     {
-        public string CreateToken(/*ApplicationUserDto user*/)
+        private readonly IdentitySetting _identitySetting;
+        public TokenHelperUtilty(IOptions<IdentitySetting> identitySetting)
         {
-            //List<Claim> claims = new()
-            //{
-            // new Claim(ClaimTypes.Name, user.UserName),
-            // new Claim(ClaimTypes.Role, "Admin")
-            //};
+            _identitySetting = identitySetting.Value;
+        }
 
-            //var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSetting:Token").Value!));
+        public string CreateToken(ApplicationUser user)
+        {
+            List<Claim> claims = new()
+            {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString())
+            };
 
-            //var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_identitySetting.SigningKey));
 
-            //var token = new JwtSecurityToken(
-            //    _tokenSetting.Issuer,
-            //    _tokenSetting.Audience,
-            //    claims: claims,
-            //    expires: DateTime.Now.AddDays(1),
-            //    signingCredentials: cred
-            // );
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-            //var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+            var token = new JwtSecurityToken(
+                _identitySetting.Issuer,
+                _identitySetting.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddDays(_identitySetting.ExpiresTime),
+                signingCredentials: cred
+             );
 
-            //return jwt;
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
 
-            return "";
+            return jwt;
         }
     }
 }
