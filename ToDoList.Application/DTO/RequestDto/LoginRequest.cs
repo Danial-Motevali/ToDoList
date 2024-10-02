89317e08942bbbd990f2a05a4ef009e0fb94ecca
Danial-Motevali <danial.motevali.82@gmail.com>
2024-10-02 09:57:27 +0330

    create a LoginRequest from end point sign in

---
+++ ToDoList.Application/DTO/RequestDto/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace ToDoList.Application.DTO.RequestDto
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
