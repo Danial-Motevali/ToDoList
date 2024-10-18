0abf73e8f3c9e95f97c03a5c7401bd9c392f7407
Danial Motevali <danial.motevali.82@gmail.com>
2024-10-18 11:27:10 +0330

    Creae a Domain

---
+++ Back-End/TaskManager.Domain/SystemEntity/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManager.Domain.SystemEntity
+{
+    public class ApplicationUser
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+
+        public virtual List<Task> Tasks { get; set; }
+    }
+}
---
+++ Back-End/TaskManager.Domain/SystemEntity/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManager.Domain.SystemEntity
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UserId { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
