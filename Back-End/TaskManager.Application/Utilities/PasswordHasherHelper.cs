26aa83d089f1584ace5e04bdab212938b93e1a2f
Danial-Motevali <danial.motevali.82@gmail.com>
2024-10-21 15:37:40 +0330

    create a password hasher and also validate

---
+++ Back-End/TaskManager.Application/Utilities/PasswordHasherHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace TaskManager.Application.Utilities
+{
+    public static class PasswordHasherHelper
+    {
+        public static string CreateHash(string password)
+        {
+            if (password == null)
+                return null;
+
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool ValidHash(string inputPassword, string userPassword)
+        {
+            if(inputPassword == null || userPassword == null)
+                return false;
+
+            return BCrypt.Net.BCrypt.Verify(inputPassword, userPassword);
+        }
+    }
+}
