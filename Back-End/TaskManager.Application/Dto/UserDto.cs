b9308a2974c4250e6f1eac14613b53abeef12d4b
Danial-Motevali <danial.motevali.82@gmail.com>
2024-10-21 15:35:44 +0330

    add one dto for user

--- Back-End/TaskManager.Application/Dto/UserDto.cs
+++ Back-End/TaskManager.Application/Dto/UserDto.cs
@@ -1,6 +1,18 @@
 ﻿namespace TaskManager.Application.Dto
 {
+    public class UserDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
     public class UserSignUpDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserSignInDto 
     {
         public string UserName { get; set; }
         public string Password { get; set; }
