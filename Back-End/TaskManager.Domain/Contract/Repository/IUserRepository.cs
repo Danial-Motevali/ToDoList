7581a4fcfda8bf47594fe8d477a4d846f3b59603
Danial-Motevali <danial.motevali.82@gmail.com>
2024-10-22 10:38:13 +0330

    dont need a specific user repo for now

--- Back-End/TaskManager.Domain/Contract/Repository/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using TaskManager.Domain.SystemEntity;
-
-namespace TaskManager.Domain.Contract.Repository
-{
-    public interface IUserRepository : IGenericRepository<ApplicationUser>
-    {
-        ApplicationUser FindTheUserWithUserName(string userName);
-    }
-}
