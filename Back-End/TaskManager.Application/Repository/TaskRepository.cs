b49153eb5cb1819d82cd5c7073f64792f025d3fd
Danial-Motevali <danial.motevali.82@gmail.com>
2024-10-22 13:13:19 +0330

    create a specific TaskRepository

---
+++ Back-End/TaskManager.Application/Repository/TaskRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Application.Data;
+using TaskManager.Domain.Contract.Repository;
+using TaskManager.Domain.SystemEntity;
+
+namespace TaskManager.Application.Repository
+{
+    public class TaskRepository : GenericRepository<UserTask>, ITaskRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public TaskRepository(ApplicationDbContext context) : base(context) 
+        {
+            _context = context;
+        }
+
+        public async Task<List<UserTask>> All(int Id)
+        {
+            return await _context.UserTasks.Where(x => x.UserId == Id && x.IsDeleted == false).OrderBy(x => x.Created).ToListAsync();
+        }
+
+        public async Task<List<UserTask>> AllAndDelete(int Id)
+        {
+            return await _context.UserTasks.Where(x => x.UserId == Id && x.IsDeleted == true).OrderBy(x => x.Created).ToListAsync();
+        }
+
+        public async Task<List<UserTask>> CompletedOnly(int Id)
+        {
+            return await _context.UserTasks.Where(x => x.UserId == Id && x.IsDeleted == false && x.IsCompleted == true).OrderBy(x => x.Created).ToListAsync();
+        }
+
+        public async Task<List<UserTask>> DeletedOnly(int Id)
+        {
+            return await _context.UserTasks.Where(x => x.UserId == Id && x.IsDeleted == true).OrderBy(x => x.Created).ToListAsync();
+        }
+
+        public async Task<List<UserTask>> InProgressOnly(int Id)
+        {
+            return await _context.UserTasks.Where(x => x.UserId == Id && x.IsDeleted == false && x.IsCompleted == false).OrderBy(x => x.Created).ToListAsync();
+        }
+    }
+}
---
+++ Back-End/TaskManager.Domain/Contract/Repository/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using TaskManager.Domain.SystemEntity;
+
+namespace TaskManager.Domain.Contract.Repository
+{
+    public interface ITaskRepository : IGenericRepository<UserTask>
+    {
+        Task<List<UserTask>> All(int Id);
+        Task<List<UserTask>> AllAndDelete(int Id);
+        Task<List<UserTask>> CompletedOnly(int Id);
+        Task<List<UserTask>> DeletedOnly(int Id);
+        Task<List<UserTask>> InProgressOnly(int Id);
+    }
+}
